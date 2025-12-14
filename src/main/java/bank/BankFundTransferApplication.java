cb61c9656d18e3c5c38560083aa01a6fafdf4d72
agathiyan <agathitechwork@gmail.com>
2025-12-14 17:41:34 +0530

    Initial commit - Spring Boot 4.0 BankFundTransfer setup via Spring Initializr

---
+++ src/main/java/bank/BankFundTransferApplication.java
@@ -0,0 +1,15 @@
+package bank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BankFundTransferApplication {
+
+	public static void main(String[] args) {
+		
+		SpringApplication.run(BankFundTransferApplication.class, args);
+		System.out.print("Fund transfer");
+	}
+
+}
